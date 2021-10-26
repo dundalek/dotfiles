49ae660a00f305940caeb02cfcb7290022f882ef
Jakub Dundalek <dundalek@gmail.com>
2021-10-26 17:31:20 +0200

    Menu: Add window related actions

--- rice/.config/awesome/rc.lua
+++ rice/.config/awesome/rc.lua
@@ -259,6 +259,13 @@ rice = {
         -- client.focus = nxt
       end
   end,
+  minimize_others = function()
+    for _, c in ipairs(awful.tag.selected():clients()) do
+      if not (c == client.focus) then
+        c.minimized = true
+      end
+    end
+  end,
 }
 
 
@@ -405,58 +412,54 @@ globalkeys = gears.table.join(
 ]]
 
 clientkeys = gears.table.join(
-    awful.key({ modkey,           }, "f",
-        function (c)
-            c.fullscreen = not c.fullscreen
-            c:raise()
-        end,
-        {description = "toggle fullscreen", group = "client"}),
-    awful.key({ modkey, }, "q",      function (c) c:kill()                         end,
-              {description = "close", group = "client"}),
-    -- awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
-    --           {description = "close", group = "client"}),
-    awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
-              {description = "toggle floating", group = "client"}),
-    awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
-              {description = "move to master", group = "client"}),
-    awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
-              {description = "move to screen", group = "client"}),
-    awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
-              {description = "toggle keep on top", group = "client"}),
-    awful.key({ modkey, "Ctrl" }, "n",
-        function (c)
-            for _, c in ipairs(awful.tag.selected():clients()) do
-              if not (c == client.focus) then
-                c.minimized = true
-              end
-            end
-        end ,
-        {description = "minimize other clients", group = "client"}),
-    awful.key({ modkey,           }, "n",
-        function (c)
-            -- The client currently has the input focus, so it cannot be
-            -- minimized, since minimized clients can't have the focus.
-            c.minimized = true
-        end ,
-        {description = "minimize", group = "client"}),
-    awful.key({ modkey,           }, "m",
-        function (c)
-            c.maximized = not c.maximized
-            c:raise()
-        end ,
-        {description = "(un)maximize", group = "client"}),
-    awful.key({ modkey, "Control" }, "m",
-        function (c)
-            c.maximized_vertical = not c.maximized_vertical
-            c:raise()
-        end ,
-        {description = "(un)maximize vertically", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "m",
-        function (c)
-            c.maximized_horizontal = not c.maximized_horizontal
-            c:raise()
-        end ,
-        {description = "(un)maximize horizontally", group = "client"})
+    -- awful.key({ modkey,           }, "f",
+        -- function (c)
+            -- c.fullscreen = not c.fullscreen
+            -- c:raise()
+        -- end,
+        -- {description = "toggle fullscreen", group = "client"}),
+    -- awful.key({ modkey, }, "q",      function (c) c:kill()                         end,
+              -- {description = "close", group = "client"}),
+    -- -- awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
+    -- --           {description = "close", group = "client"}),
+    -- awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
+              -- {description = "toggle floating", group = "client"}),
+    -- awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
+              -- {description = "move to master", group = "client"}),
+    -- awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
+              -- {description = "move to screen", group = "client"}),
+    -- awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
+              -- {description = "toggle keep on top", group = "client"}),
+    -- awful.key({ modkey, "Ctrl" }, "n",
+        -- function (c)
+          -- rice.minimize_others()
+        -- end ,
+        -- {description = "minimize other clients", group = "client"}),
+    -- awful.key({ modkey,           }, "n",
+        -- function (c)
+            -- -- The client currently has the input focus, so it cannot be
+            -- -- minimized, since minimized clients can't have the focus.
+            -- c.minimized = true
+        -- end ,
+        -- {description = "minimize", group = "client"}),
+    -- awful.key({ modkey,           }, "m",
+        -- function (c)
+            -- c.maximized = not c.maximized
+            -- c:raise()
+        -- end ,
+        -- {description = "(un)maximize", group = "client"}),
+    -- awful.key({ modkey, "Control" }, "m",
+        -- function (c)
+            -- c.maximized_vertical = not c.maximized_vertical
+            -- c:raise()
+        -- end ,
+        -- {description = "(un)maximize vertically", group = "client"}),
+    -- awful.key({ modkey, "Shift"   }, "m",
+        -- function (c)
+            -- c.maximized_horizontal = not c.maximized_horizontal
+            -- c:raise()
+        -- end ,
+        -- {description = "(un)maximize horizontally", group = "client"})
 )
 
 -- Bind all key numbers to tags.
